d96270773f76d88a2da4f6f211f8e1706e49a3a4
yasmin <yasminrhamouda@hotmail.com>
2025-02-17 17:35:02 +0000

    config and metadata

--- src/appwrite/config.ts
+++ src/appwrite/config.ts
@@ -1,20 +1,11 @@
-import { Client, Databases, ID } from "appwrite";
+import { Client, Databases } from "appwrite";
 
-const client = new Client()
+const client = new Client();
+
+client
     .setEndpoint(import.meta.env.VITE_ENDPOINT)
     .setProject(import.meta.env.VITE_PROJECT_ID);
 
 const databases = new Databases(client);
 
-const promise = databases.createDocument(
-    '<DATABASE_ID>',
-    '<COLLECTION_ID>',
-    ID.unique(),
-    { "title": "Hamlet" }
-);
-
-promise.then(function (response) {
-    console.log(response);
-}, function (error) {
-    console.log(error);
-});
+export { client, databases };
