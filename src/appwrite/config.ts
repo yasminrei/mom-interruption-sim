e2bdf1829ab1a8a1b00c59f6ebef1eacf6de3fb6
yasmin <yasminrhamouda@hotmail.com>
2025-07-26 02:47:16 +0100

    updated env variable names

--- src/appwrite/config.ts
+++ src/appwrite/config.ts
@@ -3,8 +3,8 @@ import { Client, Databases } from "appwrite";
 const client = new Client();
 
 client
-    .setEndpoint(import.meta.env.VITE_ENDPOINT)
-    .setProject(import.meta.env.VITE_PROJECT_ID);
+    .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
 const databases = new Databases(client);
 
